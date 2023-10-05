fcc4db4319ed8ebc26e0d0775913e1f0cc16548a
narasimha-snow <144354772+narasimha-snow@users.noreply.github.com>
2023-10-05 18:45:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My Family New Healthy Food ')
 streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & Millet Upma')
+streamlit.text(' 🥣 omega 3 & Millet Upma')
 streamlit.text('broownrice & egg omlet')
